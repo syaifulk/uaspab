0c7dfb5506d5bd32cf4608a0957090a8f051e483
syaifulk <khabibsyaiful0@gmail.com>
2025-07-05 20:51:13 +0700

    up9

--- resources/views/post/artikel.blade.php
+++ resources/views/post/artikel.blade.php
@@ -2,16 +2,22 @@
 @section('content')
 @foreach ($artikel as $post)
 <section class="text-gray-600 body-font">
-  <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0 mb-10">
-      <a href = "{{ route('post.detail', ['slug'=> $post->slug]) }}"><img class="object-cover object-center rounded" src="{{$post->getFirstMediaUrl('featured_image')}}" /></a>
+  <div class="container mx-auto flex px-4 py-8 md:flex-row flex-col items-center">
+    <div class="w-full md:w-1/2 lg:w-1/3 mb-4 md:mb-0">
+      <a href="{{ route('post.detail', ['slug'=> $post->slug]) }}">
+        <img class="object-cover object-center rounded-md shadow-md w-full h-48" src="{{ $post->getFirstMediaUrl('featured_image') }}" alt="{{ $post->title }}" />
+      </a>
     </div>
-    <div id ="blog-content" class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-      <a href = "{{ route('post.detail', ['slug'=> $post->slug]) }}"><h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">{{$post->title}}</h1></a>
-      <a href = "{{ route('post.detail', ['slug'=> $post->slug]) }}"><p class="mb-8 leading-relaxed">{{$post->excerpt}}</p></a>
-      <div class="flex justify-center">
-        <a href = "{{ route('post.detail', ['slug' => $post->slug]) }}" class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Lihat Selengkapnya</a>
-      </div>
+    <div class="w-full md:w-1/2 lg:w-2/3 md:pl-6">
+      <a href="{{ route('post.detail', ['slug'=> $post->slug]) }}">
+        <h1 class="text-xl font-semibold text-gray-900 mb-2">{{ $post->title }}</h1>
+      </a>
+      <a href="{{ route('post.detail', ['slug'=> $post->slug]) }}">
+        <p class="text-sm text-gray-700 mb-3">{{ $post->excerpt }}</p>
+      </a>
+      <a href="{{ route('post.detail', ['slug' => $post->slug]) }}" class="inline-block text-white bg-indigo-500 hover:bg-indigo-600 px-4 py-2 rounded text-sm">
+        Lihat Selengkapnya
+      </a>
     </div>
   </div>
 </section>
