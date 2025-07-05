c26c411cf9b73cbfb8ddc5b2543cb6231b558998
syaifulk <khabibsyaiful0@gmail.com>
2025-07-05 21:29:43 +0700

    up11

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -24,7 +24,7 @@
             <a class="mr-5 hover:text-gray-900" href="{{ route('artikel') }}">Artikel</a>
             <a class="mr-5 hover:text-gray-900" href="{{ route('penulis') }}">Penulis</a>
             </nav>
-            <a href="http://localhost:8000/admin/" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
+            <a href="https://uaspab-pabuas.up.railway.app/admin/login" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
                 <path d="M5 12h14M12 5l7 7-7 7"></path>
             </svg>
